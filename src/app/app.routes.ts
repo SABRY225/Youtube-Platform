9f7b57f54523e6717e29ca0a39b890046d6c5e25
Mohamed-Kamel11 <mohamedkamelalsayed56@gmail.com>
2024-09-12 13:58:18 +0300

    Add Page Login ,Layout and Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Auth/login/login.component';
+import { LayoutComponent } from './Auth/layout/layout.component';
+import { DashboardComponent } from './Auth/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',redirectTo:'Login',pathMatch:'full'
+    },
+    {
+        path:'Login',
+        component:LoginComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'Dashboard',
+                component:DashboardComponent
+            }
+        ]
+    }
+];
