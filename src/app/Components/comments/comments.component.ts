04f4d6995e06092eb47ca52d04d95c2d4a523cda
SABRY225 <ahmed010144497@gmail.com>
2024-09-25 06:57:57 +0300

    add responsive to project

--- src/app/Components/comments/comments.component.ts
+++ src/app/Components/comments/comments.component.ts
@@ -15,15 +15,22 @@ import { CheckLike, CountsLike, LikeData } from '../../Models/like';
 import { CommentService } from '../../Services/comment.service';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Comment } from '../../Models/comment';
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [MatSnackBarModule, MatTooltipModule,MatMenuModule,MatButtonModule],
+  imports: [MatSnackBarModule, MatTooltipModule,MatMenuModule,MatButtonModule,ReactiveFormsModule,CommonModule],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css'
 })
 export class CommentsComponent {
   commentData: any = {};
+  comment:Comment={
+    content:''
+  }
   data: ResultMessage = {
     message: '',
     success: false
@@ -38,17 +45,16 @@ export class CommentsComponent {
   };
   videoId: string = '';
   isEdit:boolean=false
+  commentForm!: FormGroup;
 
   constructor(
     private userService: UserService,
-    private videoService: VideoService,
-    private saveService: SaveService,
     private commentService: CommentService,
-    private likeService: LikeService,
-    private router: Router,
     private route: ActivatedRoute,
     private snackBar: MatSnackBar, // Inject MatSnackBar
-    private sanitizer: DomSanitizer  // Inject DomSanitizer
+    private sanitizer: DomSanitizer , // Inject DomSanitizer
+    private router: Router,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -59,18 +65,71 @@ export class CommentsComponent {
   loadUser(): void {
     this.route.params.subscribe(params => {
       this.videoId=params['videoId']
-
-
     this.userService.currentUser().subscribe((userData) => {
       this.user = userData;
     this.commentService.getComments(this.videoId).subscribe(data=>{
      this.commentData=data
     })
   });
-    
-
-
+  this.commentForm = this.fb.group({
+    content: ['', [Validators.required, Validators.minLength(3)]],
+  });
   });
 
   }
+  // more comment
+  isDown:boolean=false
+  showComment(status:string){
+    if(status=='Down'){
+      this.isDown=true
+    }else{
+      this.isDown=false
+    }
+  }
+  // add comment
+  onSubmit() {
+    if (this.commentForm.valid) {
+      console.log('Comment Submitted:', this.commentForm.value.content);
+      this.comment.content=this.commentForm.value.content
+      this.commentService.createComment(this.videoId,this.comment).subscribe(data=>{
+        this.data=data
+        if (this.data.success) {
+          this.openSnackBar(this.data.message, 'Close'); 
+          this.commentForm.reset(); 
+          this.commentService.getComments(this.videoId).subscribe(data=>{
+            this.commentData=data
+           })
+        } else {
+          this.openSnackBar(this.data.message, 'Retry');  // Show failure toast
+        }
+      })
+    } else {
+      this.openSnackBar('Form is invalid', 'Retry');  // Show failure toast
+    }
+  }
+
+  // delete comment
+
+  deleteComment(commentId:string){
+    this.commentService.deleteComment(commentId).subscribe(data=>{
+      this.data=data
+      if (this.data.success) {
+        this.openSnackBar(this.data.message, 'Close'); 
+        this.commentService.getComments(this.videoId).subscribe(data=>{
+          this.commentData=data
+         })
+      } else {
+        this.openSnackBar(this.data.message, 'Retry');  // Show failure toast
+      }
+    })
+  }
+
+
+
+      // Toast notification method
+      openSnackBar(message: string, action: string): void {
+        this.snackBar.open(message, action, {
+          duration: 3000,  // Duration of the toast (3 seconds)
+        });
+      }
 }
--- src/app/Models/comment.ts
+++ src/app/Models/comment.ts
@@ -1,2 +1,3 @@
 export interface Comment {
+    content:string
 }
